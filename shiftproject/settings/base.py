89818496096b14506d9f7f1634a8275018e6af4c
Miyu Namba <ftvocalove47@gmail.com>
2020-05-20 13:35:28 +0900

    Initial commit

--- shiftproject/settings/base.py
+++ shiftproject/settings/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -122,5 +121,3 @@ STATICFILES_DIRS = [
 LOGIN_URL = 'login'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-django_heroku.settings(locals())

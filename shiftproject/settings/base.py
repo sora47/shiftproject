7017271aa88594f009e5b6b4414758b68318e696
Miyu Namba <ftvocalove47@gmail.com>
2020-05-23 03:27:24 +0900

    first commit

--- shiftproject/settings/base.py
+++ shiftproject/settings/base.py
@@ -125,5 +125,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = 'login'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
--- shiftproject/settings/production.py
+++ shiftproject/settings/production.py
@@ -9,3 +9,5 @@ db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = ['.herokuapp.com']
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

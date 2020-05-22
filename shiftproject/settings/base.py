452e15b4a6117103a878246af3b8f8c218537db1
Miyu Namba <ftvocalove47@gmail.com>
2020-05-23 02:53:52 +0900

    first commit

--- shiftproject/settings/base.py
+++ shiftproject/settings/base.py
@@ -50,6 +50,7 @@ ROOT_URLCONF = 'shiftproject.urls'
 
 TEMPLATES = [
     {
+       
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,

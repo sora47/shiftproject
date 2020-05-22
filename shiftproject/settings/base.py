2ca2f0d8cc0a127139e712d37cd55c1297834193
Miyu Namba <ftvocalove47@gmail.com>
2020-05-23 03:14:23 +0900

    first commit

--- shiftproject/settings/base.py
+++ shiftproject/settings/base.py
@@ -121,6 +121,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 LOGIN_URL = 'login'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

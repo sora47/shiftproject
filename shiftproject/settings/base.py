e2e62f363e9011951cea1d4a1f83fd221d91d1a6
Miyu Namba <ftvocalove47@gmail.com>
2020-05-23 03:47:36 +0900

    first commit

--- shiftproject/settings/base.py
+++ shiftproject/settings/base.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'))
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 

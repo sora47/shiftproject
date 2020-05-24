2e114591033faa7ed855f34dc0871e950f3df350
Miyu Namba <ftvocalove47@gmail.com>
2020-05-25 05:50:01 +0900

    yes

--- shiftproject/settings/production.py
+++ shiftproject/settings/production.py
@@ -2,10 +2,10 @@ from .base import *
 
 SECTRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
+from dj_database_url import config
+db_from_env = config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = ['.herokuapp.com']

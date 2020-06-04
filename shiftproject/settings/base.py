188688104404f9f0c6c9aa142966576c29faf858
Miyu Namba <ftvocalove47@gmail.com>
2020-06-05 04:46:37 +0900

    aaa

--- shiftproject/settings/base.py
+++ shiftproject/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
